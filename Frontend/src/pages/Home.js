447c7f0699c1d5378c99c72e9fddc7c503fdac47
suharsh2000 <suharsh2000@gmail.com>
2025-05-19 20:06:53 +0930

    update home

--- Frontend/src/pages/Home.js
+++ Frontend/src/pages/Home.js
@@ -23,7 +23,7 @@ const Home = () => {
 
   const fetchUsers = async () => {
   try {
-    const res = await axios.get("https://fringe-booking.vercel.app/user/getAllUsers");
+    const res = await axios.get("https://adalaide-backend-1747617833788.azurewebsites.net/user/getAllUsers");
     console.log("Fetched users:", res.data); // ✅ Debug log
 
     // Fix here: access the actual array
