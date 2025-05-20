ee0ed7aa9aba87c39d9bc835d2062c7746914a78
suharsh2000 <suharsh2000@gmail.com>
2025-05-20 20:03:54 +0930

    ci-cd

--- Frontend/src/pages/Home.js
+++ Frontend/src/pages/Home.js
@@ -91,7 +91,7 @@ const Home = () => {
 
     try {
       const result = await axios.post(
-        "https://fringe-booking.vercel.app/user/register",
+        "https://adalaide-backend-1747617833788.azurewebsites.net/user/register",
         formData
       );
       if (
